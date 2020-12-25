edb20683771ff4693d3c6d451c01f300a891b154
Andres Siri <44003042+Ghelter@users.noreply.github.com>
2020-12-25 18:02:56 -0500

    Updated the app added delete on bad words
    -Added auto .lower to text messages 
    -Added list of bad words and imported then to the main.py
    -Added message delete if a bad word has been typed + gif 
    -Added Coin Flip when user enters !coin

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 import requests
 import json
 import random
+from badWords import Bad_Words
 from emoji import EMOJI_ALIAS_UNICODE as EMOJIS
+
 
 
 
@@ -14,16 +16,15 @@ sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
 
 AngryEmoji = EMOJIS[':rage:']
 
-Nono_Words = [
-  "die",
-  "hang yourself",
-]
-
 COD_Warzone = [
   "warzone",
   "bunker",
   "bunkers",
   "codes"
+]
+
+SoPa_gifs = [
+  discord.File('source.gif')
 ]
 
 COD_Images = [
@@ -37,19 +38,16 @@ start_encouragements = [
   "You got this!"
 ]
 
-
-def get_NorrisQuote():
-  response = requests.get("https://api.chucknorris.io/jokes/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]
-  return(quote)
-
 def get_InspirationQuote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = json_data[0]["q"] + " -by " + json_data[0]["a"]
   return(quote)  
 
+
+# async def coin(self, ctx):
+#  n = random.randint(0, 1)
+#  await ctx.send("Heads" if n == 1 else "Tails")
 
 @client.event
 async def on_ready():
@@ -60,30 +58,46 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  # msg = message.content  
+  # messageContent = message.content  
 
   if message.content.startswith('hello'):
     print('hello msg'.format(client))
-    await message.channel.send('Hello! Welcome to the server. say -$hello, -inspire')
+    await message.channel.send('Hello! Welcome to the server. say -$hello, -inspire, !coin to flip a coin')
 
   if message.content.startswith('inspire'):
     print('inspiration quote'.format(client))
     quote = get_InspirationQuote()
     await message.channel.send(quote)
 
-  if any(word in message.content for word in Nono_Words):
-    print('No no word has been said'.format(client))
+  if message.content.startswith('!coin'):
+    n = random.randint(0, 1)
+    if n == 1:
+      await message.channel.send("HEADS")
+    else:
+      await message.channel.send("TAILS")
+
+  if any(word in message.content.lower() for word in Bad_Words):
+    print('Bad word has been said'.format(client))
     await message.add_reaction(AngryEmoji)
     
 
-  if any(word in message.content for word in COD_Warzone):
+  if any(word in message.content.lower() for word in COD_Warzone):
     print('warzone codes image'.format(client))
     await message.channel.send(file=discord.File('CardBunkers.jpg'))
     await message.channel.send(file=discord.File('Screenshot_3.png'))
     
 
-  if any(word in message.content for word in sad_words):
+  if any(word in message.content.lower() for word in sad_words):
       await message.channel.send(random.choice(start_encouragements))
 
+  if len(message.content) > 0:
+    for word in Bad_Words:
+      if word in message.content:
+        #To delete the message from chat
+        #await message.delete()
+        await message.channel.send('Bro!')
+        await message.channel.send(file=discord.File('source.gif'))
   
+
+keep_online()
 client.run(os.getenv('TOKEN'))
